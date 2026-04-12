29deee838bdb15cec5f7fc29344fe8dd1b34723f
IvanJ.Dev <jivan8764@gmail.com>
2026-04-12 01:54:58 -0300

    temp: disable cors for testing

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import contactRouter from "./routes/contactRoutes";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
